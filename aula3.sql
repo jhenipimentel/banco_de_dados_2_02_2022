59e8ece21f69814fa17ca5cbab22b30250a4818e
JHENIFER BIANCA DE ASSIS PIMENTEL <jhenifer.2986@aluno.pr.senac.br>
2022-08-15 22:23:12 -0300

    aula 3: Sub selects de filmes e atores

---
+++ aula3.sql
@@ -0,0 +1,55 @@
+USE sakila;
+
+SET @ID_CUSTOMER_SELECTED = (SELECT
+    customer.customer_id
+FROM 
+    customer
+WHERE 
+    customer.email = 'KIM.CRUZ@sakilacustomer.org');
+
+SELECT @ID_CUSTOMER_SELECTED AS 'ID_CUSTOMER_SELECTED';
+
+
+SELECT
+    actor.actor_id,
+    actor.first_name,
+    actor.last_name
+FROM
+    actor
+WHERE
+    actor.actor_id IN(
+        SELECT
+            film_actor.film_id
+        FROM
+            film_actor
+        WHERE
+            film_actor.film_id IN(
+                SELECT
+                    film.film_id
+                FROM
+                    film
+                WHERE
+                film.film_id IN(
+                        SELECT
+                            inventory.film_id
+                        FROM
+                            inventory
+                        WHERE
+                            inventory.inventory_id IN(
+                                SELECT
+                                    rental.inventory_id
+                                FROM 
+                                    rental
+                                WHERE 
+                                    rental.customer_id = (
+                                        SELECT
+                                            customer.customer_id
+                                        FROM 
+                                            customer
+                                        WHERE 
+                                            customer.email = 'KIM.CRUZ@sakilacustomer.org'
+                                    )
+                                )
+                )           
+            )
+);
